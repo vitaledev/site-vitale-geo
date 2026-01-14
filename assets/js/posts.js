e7f4e3a2a71cdedd0746fa26cc003492f9f7b741
viteledev <agnaldotgt@gmail.com>
2026-01-14 01:48:32 -0300

    novos posts

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -14,7 +14,7 @@ const blogPosts = [
     titulo: "Ponto Nemo: O lugar mais solitário da Terra",
     categoria: "Oceanografia",
     data: "22 Jan 2026",
-    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Point_Nemo.png/640px-Point_Nemo.png",
+    img: "https://upload.wikimedia.org/wikipedia/commons/f/fd/Oceanic_pole_of_inaccessibility.png",
     link: "/blog/2026-01-22-ponto-nemo.html",
     resumo: "No Ponto Nemo, os humanos mais próximos de você não estão na Terra, mas no espaço. Conheça o cemitério de naves."
 },
@@ -22,9 +22,25 @@ const blogPosts = [
     titulo: "A Fronteira Mais Louca do Mundo",
     categoria: "Fronteiras",
     data: "25 Jan 2026",
-    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Baarle-Nassau_fronti%C3%A8re.jpg/640px-Baarle-Nassau_fronti%C3%A8re.jpg",
+    img: "https://upload.wikimedia.org/wikipedia/commons/7/7b/LocatieBaarle.jpg",
     link: "/blog/2026-01-25-fronteira-mais-louca.html",
     resumo: "Em Baarle, você pode dormir na Bélgica e tomar café na Holanda sem sair de casa. Conheça a cidade com a fronteira mais complexa do planeta."
 },
+{
+        titulo: "Ilhas Diomedes: A Fronteira do Tempo",
+        categoria: "Curiosidades",
+        data: "28 Jan 2026",
+        img: "https://upload.wikimedia.org/wikipedia/commons/0/0e/BeringSt-close-VE.jpg",
+        link: "2026-01-28-ilhas-diomedes.html", 
+        resumo: "A apenas 3,8 km de distância, estas ilhas separam os EUA da Rússia e o Hoje do Amanhã. Uma viagem onde é possível ver o futuro a olho nu."
+    },
+    {
+        titulo: "Arquivo Confidencial: Área 51",
+        categoria: "Conspiração",
+        data: "01 Fev 2026",
+        img: "https://upload.wikimedia.org/wikipedia/commons/3/3e/Area_51_Main_Gate_%28cropped%29.jpg",
+        link: "2026-02-01-area-51.html",
+        resumo: "Relatório desclassificado sobre a base militar mais secreta do mundo. [DADOS EXPURGADOS]"
+    },
     // COPIE E COLE ESTE BLOCO PARA ADICIONAR NOVO POST
 ];
