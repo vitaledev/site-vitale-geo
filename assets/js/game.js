2a56f4e080420bc044094191509e9ca818a2bb61
viteledev <agnaldotgt@gmail.com>
2026-01-14 21:31:26 -0300

    novas alterações

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,24 +4,24 @@ const gamesDB = [
         titulo: "Quiz Geopolítico",
         desc: "Teste seu conhecimento sobre capitais, conflitos e economia.",
         icon: "https://img.icons8.com/?size=100&id=XsvEZR0h6fav&format=png&color=000000", // Ícone de Cérebro
-        link: "quiz.html" // Link relativo (dentro da pasta jogos)
+        link: "/jogos/quiz" // Link relativo (dentro da pasta jogos)
     },
     {
         titulo: "Operação Bandeira",
         desc: "Você consegue identificar o país apenas pela bandeira?",
         icon: "https://img.icons8.com/?size=100&id=o6oNyiaQhfex&format=png&color=000000", // Ícone de Bandeira
-        link: "bandeiras.html"
+        link: "/jogos/bandeiras"
     },
     {
         titulo: "Guerra de Números",
         desc: "Quem é maior? Quem é mais rico? Um jogo de 'Maior ou Menor'.",
         icon: "https://img.icons8.com/?size=100&id=rOdRrQQZESUB&format=png&color=000000", // Ícone de Gráfico
-        link: "maior-menor.html"
+        link: "/jogos/maior-menor"
     },
     {
         titulo: "Missão Capital",
         desc: "Descubra a capital estratégica de cada nação.",
         icon: "https://img.icons8.com/?size=100&id=36839&format=png&color=000000", // Ícone de Cidade
-        link: "capitais.html"
+        link: "/jogos/capitais"
     },
 ];
--- assets/js/posts.js
+++ assets/js/posts.js
@@ -2,12 +2,13 @@
 // Adicione novos posts aqui no topo da lista. O site atualiza sozinho.
 
 const blogPosts = [
+    
     {
         titulo: "Principado de Sealand: A Menor Nação",
         categoria: "Curiosidades",
         data: "15 Fev 2026",
         img: "https://upload.wikimedia.org/wikipedia/commons/f/f9/Sealand_fortress.jpg",
-        link: "2026-02-15-sealand.html",
+        link: "sealand",
         resumo: "Conheça a plataforma de petróleo que virou um país, emitiu passaportes e travou uma guerra contra helicópteros."
     },  
     {
@@ -15,7 +16,7 @@ const blogPosts = [
         categoria: "Urbanismo",
         data: "20 Fev 2026",
         img: "https://upload.wikimedia.org/wikipedia/commons/7/71/Back_to_Whittier...Sun_is_out...Water_is_Rough_%282571032531%29.jpg",
-        link: "2026-02-20-whittier.html",
+        link: "whittier",
         resumo: "Conheça a cidade no Alasca onde toda a população mora no mesmo edifício e o único acesso é um túnel que fecha à noite."
     },
     {
@@ -23,7 +24,7 @@ const blogPosts = [
         categoria: "Cartografia",
         data: "20 Jan 2026",
         img: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Mercator_projection_SW.jpg/640px-Mercator_projection_SW.jpg",
-        link: "/blog/2026-01-20-farsa-mercator.html",
+        link: "farsa-mercator",
         resumo: "Por que a Groenlândia parece maior que a África? Entenda a distorção matemática dos mapas."
     },
     {
@@ -31,7 +32,7 @@ const blogPosts = [
     categoria: "Oceanografia",
     data: "22 Jan 2026",
     img: "https://upload.wikimedia.org/wikipedia/commons/f/fd/Oceanic_pole_of_inaccessibility.png",
-    link: "/blog/2026-01-22-ponto-nemo.html",
+    link: "ponto-nemo",
     resumo: "No Ponto Nemo, os humanos mais próximos de você não estão na Terra, mas no espaço. Conheça o cemitério de naves."
 },
 {
@@ -39,7 +40,7 @@ const blogPosts = [
     categoria: "Fronteiras",
     data: "25 Jan 2026",
     img: "https://upload.wikimedia.org/wikipedia/commons/7/7b/LocatieBaarle.jpg",
-    link: "/blog/2026-01-25-fronteira-mais-louca.html",
+    link: "fronteira-mais-louca",
     resumo: "Em Baarle, você pode dormir na Bélgica e tomar café na Holanda sem sair de casa. Conheça a cidade com a fronteira mais complexa do planeta."
 },
 {
@@ -47,7 +48,7 @@ const blogPosts = [
         categoria: "Curiosidades",
         data: "28 Jan 2026",
         img: "https://upload.wikimedia.org/wikipedia/commons/0/0e/BeringSt-close-VE.jpg",
-        link: "2026-01-28-ilhas-diomedes.html", 
+        link: "ilhas-diomedes", 
         resumo: "A apenas 3,8 km de distância, estas ilhas separam os EUA da Rússia e o Hoje do Amanhã. Uma viagem onde é possível ver o futuro a olho nu."
     },
     {
@@ -55,7 +56,7 @@ const blogPosts = [
         categoria: "Conspiração",
         data: "01 Fev 2026",
         img: "https://upload.wikimedia.org/wikipedia/commons/3/3e/Area_51_Main_Gate_%28cropped%29.jpg",
-        link: "2026-02-01-area-51.html",
+        link: "area-51",
         resumo: "Relatório desclassificado sobre a base militar mais secreta do mundo. [DADOS EXPURGADOS]"
     },
     
