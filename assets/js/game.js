6be323774ee870178c6850689555a20a355e1386
viteledev <agnaldotgt@gmail.com>
2026-01-18 18:36:15 -0300

    Alterações

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,24 +4,31 @@ const gamesDB = [
         titulo: "Quiz Geopolítico",
         desc: "Teste seu conhecimento sobre capitais, conflitos e economia.",
         icon: "https://img.icons8.com/?size=100&id=XsvEZR0h6fav&format=png&color=000000", // Ícone de Cérebro
+        cover: "https://i.natgeofe.com/k/c821a1da-3cea-4f22-a913-1018461702f3/quiz_geography_16x9.jpg?w=1200",
         link: "/jogos/quiz/" // Link para página de apresentação
     },
     {
-        titulo: "Operação Bandeira",
+        titulo: "Acerte a Bandeira",
         desc: "Você consegue identificar o país apenas pela bandeira?",
         icon: "https://img.icons8.com/?size=100&id=o6oNyiaQhfex&format=png&color=000000", // Ícone de Bandeira
+        cover: "https://i.ytimg.com/vi/o5TQkVy0KWM/maxresdefault.jpg",
         link: "/jogos/bandeiras/"
     },
     {
         titulo: "Guerra de Números",
         desc: "Quem é maior? Quem é mais rico? Um jogo de 'Maior ou Menor'.",
         icon: "https://img.icons8.com/?size=100&id=rOdRrQQZESUB&format=png&color=000000", // Ícone de Gráfico
+        cover: "https://upload.wikimedia.org/wikipedia/commons/7/7b/Ranks_of_countries_by_the_Good_Country_Index_1.5_%282020%29.png",
         link: "/jogos/maior-menor/"
     },
     {
-        titulo: "Missão Capital",
+        titulo: "Descubra a capital",
         desc: "Descubra a capital estratégica de cada nação.",
         icon: "https://img.icons8.com/?size=100&id=36839&format=png&color=000000", // Ícone de Cidade
+        cover: "https://i.ytimg.com/vi/EPWgsC6Tn00/maxresdefault.jpg",
         link: "/jogos/capitais/"
     },
 ];
+
+
+
--- assets/js/tools.js
+++ assets/js/tools.js
@@ -1,26 +1,30 @@
 const toolsDB = [
     {
-        titulo: "Comparador",
-        desc: "Tamanho real dos países.",
+        titulo: "Qual o Real Tamanho dos Países?",
+        desc: "Compare o tamanho real dos territórios de cada país no mapa",
         icon: "ph-bold ph-globe-stand",
+        cover: "https://s2.glbimg.com/MQkTQrC9EICVaSQa2ddkcy_RsNg=/e.glbimg.com/og/ed/f/original/2020/06/10/a2.png",
         link: "/ferramentas/comparador-paises/"
     },
     {
         titulo: "Fuso Horário",
         desc: "Calculadora global.",
         icon: "ph-bold ph-clock",
+        cover: "https://guiadoestudante.abril.com.br/wp-content/uploads/sites/4/2016/07/fusohorario.png?resize=1080,565&crop=1",
         link: "/ferramentas/calculadora-fuso/"
     },
     {
         titulo: "Gerador ABNT",
-        desc: "Citação rápida.",
+        desc: "Citações rápidas no formato ABNT.",
         icon: "ph-bold ph-quotes",
+        cover: "https://tccdescomplicado.com.br/wp-content/uploads/2022/01/GERADOR-DE-REFERENCIAS-E-CITACOES-ABNT.png",
         link: "/ferramentas/gerador-abnt/"
     },
     {
-        titulo: "Distância",
-        desc: "Medidor de rotas.",
+        titulo: "Medidor de distância",
+        desc: "Descubra a distância entre dois pontos no mapa.",
         icon: "ph-bold ph-path",
+        cover: "https://articles-images.sftcdn.net/wp-content/uploads/sites/7/2014/09/Measure-Distances-in-Google-Maps-header1.jpg",
         link: "/ferramentas/calculadora-distancia/"
     }
 ];
