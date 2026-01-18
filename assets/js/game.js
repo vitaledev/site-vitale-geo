3d5a009cb18d493449cd26477cf93f6d77595f11
viteledev <agnaldotgt@gmail.com>
2026-01-18 17:22:44 -0300

    novos posts e alterações

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,24 +4,24 @@ const gamesDB = [
         titulo: "Quiz Geopolítico",
         desc: "Teste seu conhecimento sobre capitais, conflitos e economia.",
         icon: "https://img.icons8.com/?size=100&id=XsvEZR0h6fav&format=png&color=000000", // Ícone de Cérebro
-        link: "/jogos/quiz" // Link relativo (dentro da pasta jogos)
+        link: "/jogos/quiz/" // Link para página de apresentação
     },
     {
         titulo: "Operação Bandeira",
         desc: "Você consegue identificar o país apenas pela bandeira?",
         icon: "https://img.icons8.com/?size=100&id=o6oNyiaQhfex&format=png&color=000000", // Ícone de Bandeira
-        link: "/jogos/bandeiras"
+        link: "/jogos/bandeiras/"
     },
     {
         titulo: "Guerra de Números",
         desc: "Quem é maior? Quem é mais rico? Um jogo de 'Maior ou Menor'.",
         icon: "https://img.icons8.com/?size=100&id=rOdRrQQZESUB&format=png&color=000000", // Ícone de Gráfico
-        link: "/jogos/maior-menor"
+        link: "/jogos/maior-menor/"
     },
     {
         titulo: "Missão Capital",
         desc: "Descubra a capital estratégica de cada nação.",
         icon: "https://img.icons8.com/?size=100&id=36839&format=png&color=000000", // Ícone de Cidade
-        link: "/jogos/capitais"
+        link: "/jogos/capitais/"
     },
 ];
